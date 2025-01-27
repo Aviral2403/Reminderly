526415e5f3da28f05537b974e9e7ee33107f5041
Aviral2403 <aviralkhandelwal03@gmail.com>
2025-01-28 03:24:14 +0530

    Updated Frontend URL

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -153,7 +153,7 @@ const forgotPassword = async (req, res) => {
 		await user.save();
 
 		// send email
-		await sendPasswordResetEmail(user.email, `http://localhost:5173/reset-password/${resetToken}`);
+		await sendPasswordResetEmail(user.email, `https://reminderly.onrender.com/reset-password/${resetToken}`);
 
 		res.status(200).json({ success: true, message: "Password reset link sent to your email" });
 	} catch (error) {
--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173","http://localhost:5175"],
+    origin: ["http://localhost:5174", "http://localhost:5173","http://localhost:5175","https://reminderly.onrender.com"],
     credentials: true,
     exposedHeaders: ["Authorization"],
   })
